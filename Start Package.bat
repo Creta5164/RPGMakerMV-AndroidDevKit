a306f3dfb10b6b08bf4ea84fb7bf7f85c9a95b12
Creta5164 <creta5164@gmail.com>
2019-02-09 21:12:16 +0900

    fix batch file

--- Start Package.bat
+++ Start Package.bat
@@ -1,2 +1,2 @@
-start /d "" ApplicationPackager/bin/Release/netcoreapp2.2/win-x64/publish/ApplicationPackager.exe
+start ApplicationPackager/bin/Release/netcoreapp2.2/win-x64/publish/ApplicationPackager.exe
 exit
